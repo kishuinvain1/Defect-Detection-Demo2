bdea592dbd2945f084c512a0c437b27232bc2485
kishuinvain1 <37648429+kishuinvain1@users.noreply.github.com>
2023-06-09 14:28:30 +0530

    Update streamlit_app_predict.py

--- streamlit_app_predict.py
+++ streamlit_app_predict.py
@@ -98,7 +98,7 @@ def main():
 	
 	
 	
-        results = predict(model, image)
+        results = predict(model, svd_img)
         #results = predict(model2, "main_image.jpg")
         print("Prediction Results are...")	
         print(results)
