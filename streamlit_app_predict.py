3a5c2eba8e0ba8f2ba6dfec5ff564351c12e9ef5
kishuinvain1 <37648429+kishuinvain1@users.noreply.github.com>
2023-06-09 15:23:00 +0530

    Update streamlit_app_predict.py

--- streamlit_app_predict.py
+++ streamlit_app_predict.py
@@ -23,7 +23,7 @@ def load_image():
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
         img_array = np.array(image).astype(np.float32)
-	img_array.save("main_image.jpg")
+        img_array.save("main_image.jpg")
         
        
         #opencv_image = cv2.imdecode(img_array, 1)
